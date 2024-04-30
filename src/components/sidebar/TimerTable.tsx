3c3fc0725db2cdc17a1b0a3ba4ef514e42374967
Steven Luo <luosteven23@gmail.com>
2024-04-30 17:42:37 -0600

    Finish ao5 feature

--- src/components/sidebar/TimerTable.tsx
+++ src/components/sidebar/TimerTable.tsx
@@ -3,22 +3,30 @@ import { Solve } from "../../lib";
 
 const TimerTable: React.FC<{ solves: Solve[] }> = ({ solves }) => {
   return (
-    <div className="h-[66svh] overflow-auto">
-      <table className="border-solid border-2 w-full table-fixed min-h-10">
+    <div className="h-[66svh] overflow-auto no-scrollbar">
+      <table className="w-full table-fixed min-h-10">
         <thead>
-          <tr className="border-solid border-2">
-            <th colSpan={2}>Solves: {solves.length}</th>
+          <tr>
+            <th colSpan={3}>Solves: {solves.length}</th>
+          </tr>
+          <tr>
+            <th>#</th>
+            <th>time</th>
+            <th>ao5</th>
           </tr>
         </thead>
         <tbody>
-          {/* Render list of rows using solve # as first data cell, and time for second data cell*/}
-          {solves.map((solve) => (
+          {/* Render list of rows using solve # as first data cell, time for second data cell, and ao5 for third data cell */}
+          {[...solves].reverse().map((solve) => (
             <tr key={solve.count}>
-              <td className="border-solid border-2 text-center leading-10">
+              <td className="text-center leading-10">
                 <div className="h-10">{solve.count}</div>
               </td>
-              <td className="border-solid border-2 text-center leading-10">
+              <td className="text-center leading-10">
                 <div className="h-10">{(Math.floor(solve.time / 10) / 100).toFixed(2)}</div>
+              </td>
+              <td className="text-center leading-10">
+                <div className="h-10">{solve.ao5 ? (Math.floor(solve.ao5 / 10) / 100).toFixed(2) : "-"}</div>
               </td>
             </tr>
           ))}
