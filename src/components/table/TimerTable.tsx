ae8360d19f7979bfb7387bd5d63511ebd112da56
Steven Luo <luosteven23@gmail.com>
2024-05-06 21:23:54 -0600

    Update max table height on mobile

--- src/components/table/TimerTable.tsx
+++ src/components/table/TimerTable.tsx
@@ -7,7 +7,7 @@ const TimerTable: React.FC<{ solves: Solve[]; clearSolves: () => void }> = ({ so
   return (
     <div className="custom-border absolute bottom-0 left-0 top-[13.5rem] z-20 space-y-3 border-orange-200 bg-orange-300 max-[768px]:bottom-[11.8vw] max-[768px]:top-auto">
       <Button onClick={clearSolves}>Clear Solves</Button>
-      <div className="no-scrollbar h-[90%] touch-pan-y overflow-y-auto overflow-x-hidden max-[768px]:max-h-[10.7rem]">
+      <div className="no-scrollbar h-[90%] touch-pan-y overflow-y-auto overflow-x-hidden max-[768px]:max-h-[9.5rem]">
         <table className="w-full max-w-[13.5rem] table-fixed">
           <thead>
             <tr>
